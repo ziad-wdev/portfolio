587b63122d03e065c25e1c116e64937b1f81481c
ziad-wdev <ziadahmed2371@gmail.com>
2026-03-17 15:34:30 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <Home></Home>
+      <About></About>
     </>
   );
 }
